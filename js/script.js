4c96be1cf5f3c843da0cfc48564a3fd545673f63
JK-00012 <adrianrpgbits@gmail.com>
2026-05-29 23:59:59 -0500

    aside index funcional

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 
 async function cargarArticulo() {
@@ -35,11 +36,49 @@ async function cargarArticulo() {
 
 cargarArticulo();
 
+*/
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
+
+async function cargarArticulo() {
+
+    const params = new URLSearchParams(window.location.search);
+
+    const tema = params.get("tema");
+
+    const response = await fetch(`./articulos/biologia/${tema}.md`);
+
+    const markdown = await response.text();
+
+    const html = marked.parse(markdown);
+
+    document.getElementById("content").innerHTML = html;
+
+    const headings = document.querySelectorAll("#content h2");
+
+    const aside = document.getElementById("sidebar");
+
+    headings.forEach((heading, index) => {
+
+        const id = `section-${index}`;
+
+        heading.id = id;
+
+        const link = document.createElement("a");
+
+        link.href = `#${id}`;
+
+        link.textContent = heading.textContent;
+
+        aside.appendChild(link);
+
+        aside.appendChild(document.createElement("br"));
+    });
+}
+
+cargarArticulo();
 
 
-
-
-
+// codigo nuevo
 const btn = document.getElementById("toggleBtn");
 const sidebar = document.querySelector("aside");
 
